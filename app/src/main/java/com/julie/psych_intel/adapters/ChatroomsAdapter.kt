a2b5462cbcd4a3774159cd6ff2c76de94eea007c
Julie <lokoc2623@gmail.com>
2021-07-08 13:34:41 +0300

    added chatrooms adapter

---
+++ app/src/main/java/com/julie/psych_intel/adapters/ChatroomsAdapter.kt
@@ -0,0 +1,40 @@
+package com.julie.psych_intel.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.julie.psych_intel.ChatroomProto
+import com.julie.psych_intel.databinding.ChatroomItemBinding
+
+class ChatroomsAdapter : RecyclerView.Adapter<ChatroomsAdapter.ChatroomsViewHolder>() {
+
+    private val items = ArrayList<ChatroomProto.Chatroom>()
+
+    fun setItems(items: List<ChatroomProto.Chatroom>) {
+        this.items.clear()
+        this.items.addAll(items)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatroomsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ChatroomItemBinding.inflate(inflater, parent, false)
+        return ChatroomsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ChatroomsViewHolder, position: Int) =
+        holder.bind(items[position])
+
+
+    override fun getItemCount(): Int = items.size
+
+    inner class ChatroomsViewHolder(private val binding: ChatroomItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(chatroom: ChatroomProto.Chatroom) {
+            binding.chatroom = chatroom
+            binding.executePendingBindings()
+        }
+    }
+
+}
+
